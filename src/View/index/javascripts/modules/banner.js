7704bf6e70cc2b0bb8da1dec6ecb4a62bd1c9cc3
admin166 <1664549303@qq.com>
2018-09-15 20:55:44 +0800

    wdada

--- src/View/index/javascripts/modules/banner.js
+++ src/View/index/javascripts/modules/banner.js
@@ -1,4 +1,4 @@
-alert()
+
 console.log('banner');
 var ul = $(".center .u li");
 //轮播图
@@ -23,22 +23,25 @@ function on(){
     }
     list.eq(count).addClass("on").siblings().removeClass("on");
 }
-//var timer2 = setInterval(on,2000);
+var timer2 = setInterval(on,2000);
 
 var dis = $(".m_wnews .m_box");
 var a = 0;
 
 function top(){
-    
-dis.animate({"top":-38*a},2000,function(){
-    if( dis.offset().top == -180 ){
-       
-        dis.css("top",0)
-    }
-})
-    
-    
-       
+  a++; 
+  if( a == 5 ){
+    //alert()
    
+    a = 1;
+    dis.css("top",-35)
+   //dis.animate({"top":0},1500)
+  } 
+   
+dis.stop().animate({"top":-35*a},1500)
+   
+
 }
-//var timer3 = setInterval(top,1500)
+ var timer3 = setInterval(top,1500);
+   
+
--- src/View/index/javascripts/modules/header.js
+++ src/View/index/javascripts/modules/header.js
@@ -6,4 +6,55 @@ const nav = require('./nav')
 
 nav()
 
-console.log('header')
+var ul = $(".all-hook ul li");
+ul.hover(function(){
+    $(this).css({"background-color":"skyblue","opavity":0.7})
+    $(this).find(".pop").show()
+},function(){
+    $(this).css({"background-color":"rgba(50,50,50,0.9)"})
+    ul.find(".pop").hide()
+})
+
+
+var li = ul.find(".tx a");
+li.hover(function(){
+    $(this).css("color","#ff6600").siblings().css("color","#fff")
+},function(){
+    $(this).css("color","#fff")
+})
+
+
+var litwo = ul.find("dl a");
+litwo.hover(function(){
+    $(this).css("color","#ff6600").siblings().css("color","#999")
+},function(){
+    $(this).css("color","#999")
+})
+
+//鼠标滑过
+var list = $(".bd li");
+list.hover(function(){
+  $(this).find("a").addClass("active").siblings().removeClass("active");
+},function(){
+    $(this).find("a").removeClass("active");
+})
+
+//滚轮
+$(window).scroll(function(){
+ var h = document.documentElement.scrollTop || document.body.scrollTop;
+ console.log(h)
+ if( h > 350 ){
+     $(".footer").show()
+ }else{
+    $(".footer").hide()
+ }
+});
+
+//我的信息
+$(".user").hover(function(){
+    $(".r-top-h").show();
+},function(){
+    $(".r-top-h").hide();
+})
+
+
