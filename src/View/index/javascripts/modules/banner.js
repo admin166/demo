aeb8af6d888ad06405490754b226254de09e6c4b
admin166 <1664549303@qq.com>
2018-09-15 13:34:30 +0800

    23

--- src/View/index/javascripts/modules/banner.js
+++ src/View/index/javascripts/modules/banner.js
@@ -1,4 +1,4 @@
-
+alert()
 console.log('banner');
 var ul = $(".center .u li");
 //轮播图
@@ -24,3 +24,21 @@ function on(){
     list.eq(count).addClass("on").siblings().removeClass("on");
 }
 var timer2 = setInterval(on,2000);
+
+var dis = $(".m_wnews .m_box");
+var a = 0;
+
+function top(){
+    
+dis.animate({"top":-38*a},2000,function(){
+    if( dis.offset().top == -180 ){
+       
+        dis.css("top",0)
+    }
+})
+    
+    
+       
+   
+}
+var timer3 = setInterval(top,1500)
