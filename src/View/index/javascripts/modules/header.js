4a94cd1d240e89b9b945824e1fb4d4bf8b2a88e8
admin166 <1664549303@qq.com>
2018-09-16 14:15:09 +0800

    hahaha

--- src/View/index/javascripts/modules/header.js
+++ src/View/index/javascripts/modules/header.js
@@ -84,5 +84,39 @@ $(".kf").hover(function(){
 })
 
 
+//购物车
+$("#shopcart").hover(function(){
+    $(this).find("i").css("color","#fff")
+    $(this).addClass("active2");
+},function(){
+    $(this).find("i").css("color","#ff6600")
+    $(this).removeClass("active2");
+})
+$("#shopcart").click(function(){
+  $(".allbox").animate({"right":0},1000)
+  $(".quick_links").show();
+})
 
 
+//关闭购物车
+$(".header2 .img").click(function(){
+    $(".allbox").animate({"right":-300},1000,function(){
+        $(".quick_links").hide();
+    });
+    
+})
+
+
+//导航栏
+$(".footer li").hover(function(){
+    $(this).find("a").addClass("active2").siblings().removeClass("active2");
+},function(){
+    $(this).find("a").removeClass("active2");
+})
+
+
+//滚回顶部
+$(".top").click(function(){
+    alert()
+    $("html,body").animate({"scrollTop":0},1000);
+})
